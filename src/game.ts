a96745d986d6e09d4c175ca6ef84fb319703a62b
DHIBA Ilyes Wassim (EXT) <dhibail@cib.net>
2022-11-25 09:21:16 +0100

    transfert to mac

--- src/game.ts
+++ src/game.ts
@@ -4,6 +4,10 @@ import Script1 from "../d5ee9a47-8484-4824-a609-996298830b51/src/item"
 import Script2 from "../12b03cb6-2066-4f72-b8ef-27bb851de48e/src/item"
 import Script3 from "../f89ab04f-46ef-42ea-912b-b194eb8d2f02/src/item"
 import Script4 from "../1746d432-ba19-4b2e-819f-d16cf5a64236/src/item"
+import Script5 from "../28682b28-1752-4f56-9683-adb5a325a285/src/item"
+import Script6 from "../b88efbbf-2a9a-47b4-86e1-e38ecc2b433b/src/item"
+import Script7 from "../ab84996d-dcdc-429c-818e-a7640239c803/src/item"
+import Script8 from "../c1e126e7-374e-4fe3-aaae-b4211c321cf3/src/item"
 
 const _scene = new Entity('_scene')
 engine.addEntity(_scene)
@@ -228,7 +232,7 @@ wallPlainGlass7.addComponentOrReplace(gltfShape3)
 const transform20 = new Transform({
   position: new Vector3(0, 4.5, 13.5),
   rotation: new Quaternion(-1.5394153601527394e-15, -0.7071068286895752, 8.429369557916289e-8, 0.7071068286895752),
-  scale: new Vector3(5.950501441955566, 1.1302199363708496, 0.7499995827674866)
+  scale: new Vector3(6.0127081871032715, 1.1302199363708496, 0.749998927116394)
 })
 wallPlainGlass7.addComponentOrReplace(transform20)
 
@@ -390,6 +394,230 @@ const transform33 = new Transform({
 })
 videoScreenStanding.addComponentOrReplace(transform33)
 
+const floorGlass = new Entity('floorGlass')
+engine.addEntity(floorGlass)
+floorGlass.setParent(_scene)
+const transform34 = new Transform({
+  position: new Vector3(0, 4, 2.5),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(-3.9306910037994385, 17, -3.3611326217651367)
+})
+floorGlass.addComponentOrReplace(transform34)
+floorGlass.addComponentOrReplace(gltfShape5)
+
+const concreteRoundedTable = new Entity('concreteRoundedTable')
+engine.addEntity(concreteRoundedTable)
+concreteRoundedTable.setParent(_scene)
+const transform35 = new Transform({
+  position: new Vector3(5, 4.5, 12),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1.5, 1.3669129610061646, 1.5)
+})
+concreteRoundedTable.addComponentOrReplace(transform35)
+const gltfShape8 = new GLTFShape("9d9c165d-3aaa-44ae-89f6-20d977122fce/Table_04/Table_04.glb")
+gltfShape8.withCollisions = true
+gltfShape8.isPointerBlocker = true
+gltfShape8.visible = true
+concreteRoundedTable.addComponentOrReplace(gltfShape8)
+
+const chairHighBack2 = new Entity('chairHighBack2')
+engine.addEntity(chairHighBack2)
+chairHighBack2.setParent(_scene)
+const gltfShape9 = new GLTFShape("9cc4b4a2-46b6-4b49-aef9-da3959de0ac8/HighBackChair.glb")
+gltfShape9.withCollisions = true
+gltfShape9.isPointerBlocker = true
+gltfShape9.visible = true
+chairHighBack2.addComponentOrReplace(gltfShape9)
+const transform36 = new Transform({
+  position: new Vector3(5, 4.5, 11),
+  rotation: new Quaternion(4.440840747128199e-16, 0.19509033858776093, -2.3256578884911505e-8, 0.9807853698730469),
+  scale: new Vector3(1.5000003576278687, 1.2774569988250732, 1.5000003576278687)
+})
+chairHighBack2.addComponentOrReplace(transform36)
+
+const chairHighBack3 = new Entity('chairHighBack3')
+engine.addEntity(chairHighBack3)
+chairHighBack3.setParent(_scene)
+chairHighBack3.addComponentOrReplace(gltfShape9)
+const transform37 = new Transform({
+  position: new Vector3(3.5, 4.5, 12),
+  rotation: new Quaternion(1.195820997901124e-15, -0.8314696550369263, 9.911889975455779e-8, -0.5555703043937683),
+  scale: new Vector3(1.5000014305114746, 1.197467565536499, 1.5000014305114746)
+})
+chairHighBack3.addComponentOrReplace(transform37)
+
+const chairHighBack4 = new Entity('chairHighBack4')
+engine.addEntity(chairHighBack4)
+chairHighBack4.setParent(_scene)
+chairHighBack4.addComponentOrReplace(gltfShape9)
+const transform38 = new Transform({
+  position: new Vector3(4.5, 4.5, 13.5),
+  rotation: new Quaternion(-5.725116442799024e-15, -0.9807853698730469, 1.1691872003893877e-7, 0.19509033858776093),
+  scale: new Vector3(1.5000072717666626, 1.1997764110565186, 1.5000072717666626)
+})
+chairHighBack4.addComponentOrReplace(transform38)
+
+const chairHighBack6 = new Entity('chairHighBack6')
+engine.addEntity(chairHighBack6)
+chairHighBack6.setParent(_scene)
+chairHighBack6.addComponentOrReplace(gltfShape9)
+const transform39 = new Transform({
+  position: new Vector3(6, 4.5, 12),
+  rotation: new Quaternion(-1.0889569919349164e-15, -0.5555702447891235, 6.622912707143769e-8, 0.8314696550369263),
+  scale: new Vector3(1.5000016689300537, 1.3803800344467163, 1.5000016689300537)
+})
+chairHighBack6.addComponentOrReplace(transform39)
+
+const galleryInfoDarkGrey2 = new Entity('galleryInfoDarkGrey2')
+engine.addEntity(galleryInfoDarkGrey2)
+galleryInfoDarkGrey2.setParent(_scene)
+const transform40 = new Transform({
+  position: new Vector3(0.5, 5, 4.5),
+  rotation: new Quaternion(-1.5394153601527394e-15, 0.7071068286895752, -8.429369557916289e-8, 0.7071068286895752),
+  scale: new Vector3(2.0000038146972656, 1, 1.0000019073486328)
+})
+galleryInfoDarkGrey2.addComponentOrReplace(transform40)
+
+const externalLink = new Entity('externalLink')
+engine.addEntity(externalLink)
+externalLink.setParent(_scene)
+const transform41 = new Transform({
+  position: new Vector3(0.35857200622558594, 6, 2.5),
+  rotation: new Quaternion(-5.861544757360603e-16, 0.7071068286895752, -8.429369557916289e-8, 0.7071068286895752),
+  scale: new Vector3(2.000004768371582, 3, 2.000004768371582)
+})
+externalLink.addComponentOrReplace(transform41)
+
+const plainText = new Entity('plainText')
+engine.addEntity(plainText)
+plainText.setParent(_scene)
+const transform42 = new Transform({
+  position: new Vector3(16, 2.5, 11.5),
+  rotation: new Quaternion(2.980232594040899e-8, 0.7071068286895752, -1.3411046495548362e-7, 0.7071068286895752),
+  scale: new Vector3(2.9238779544830322, 5.3441667556762695, 1.257036566734314)
+})
+plainText.addComponentOrReplace(transform42)
+
+const galleryInfoBlack = new Entity('galleryInfoBlack')
+engine.addEntity(galleryInfoBlack)
+galleryInfoBlack.setParent(_scene)
+const transform43 = new Transform({
+  position: new Vector3(13.629700660705566, 0, 15.5),
+  rotation: new Quaternion(-4.693676376114463e-15, 1, -1.1920928244535389e-7, -5.960464477539063e-8),
+  scale: new Vector3(0.8168486952781677, 1, 0.9999999403953552)
+})
+galleryInfoBlack.addComponentOrReplace(transform43)
+
+const concreteRoundedTable2 = new Entity('concreteRoundedTable2')
+engine.addEntity(concreteRoundedTable2)
+concreteRoundedTable2.setParent(_scene)
+concreteRoundedTable2.addComponentOrReplace(gltfShape8)
+const transform44 = new Transform({
+  position: new Vector3(11, 4.5, 12),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1.5, 1.3669129610061646, 1.5)
+})
+concreteRoundedTable2.addComponentOrReplace(transform44)
+
+const concreteRoundedTable3 = new Entity('concreteRoundedTable3')
+engine.addEntity(concreteRoundedTable3)
+concreteRoundedTable3.setParent(_scene)
+concreteRoundedTable3.addComponentOrReplace(gltfShape8)
+const transform45 = new Transform({
+  position: new Vector3(13, 4.5, 6.5),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1.5, 1.3669129610061646, 1.5)
+})
+concreteRoundedTable3.addComponentOrReplace(transform45)
+
+const chairHighBack = new Entity('chairHighBack')
+engine.addEntity(chairHighBack)
+chairHighBack.setParent(_scene)
+chairHighBack.addComponentOrReplace(gltfShape9)
+const transform46 = new Transform({
+  position: new Vector3(11.5, 4.5, 11),
+  rotation: new Quaternion(4.440840747128199e-16, 0.19509033858776093, -2.3256578884911505e-8, 0.9807853698730469),
+  scale: new Vector3(1.5000003576278687, 1.2774569988250732, 1.5000003576278687)
+})
+chairHighBack.addComponentOrReplace(transform46)
+
+const chairHighBack5 = new Entity('chairHighBack5')
+engine.addEntity(chairHighBack5)
+chairHighBack5.setParent(_scene)
+chairHighBack5.addComponentOrReplace(gltfShape9)
+const transform47 = new Transform({
+  position: new Vector3(12, 4.5, 12),
+  rotation: new Quaternion(-1.0889569919349164e-15, -0.5555702447891235, 6.622912707143769e-8, 0.8314696550369263),
+  scale: new Vector3(1.5000019073486328, 1.3803800344467163, 1.5000019073486328)
+})
+chairHighBack5.addComponentOrReplace(transform47)
+
+const chairHighBack7 = new Entity('chairHighBack7')
+engine.addEntity(chairHighBack7)
+chairHighBack7.setParent(_scene)
+chairHighBack7.addComponentOrReplace(gltfShape9)
+const transform48 = new Transform({
+  position: new Vector3(11, 4.5, 13),
+  rotation: new Quaternion(-5.725116442799024e-15, -0.9807853698730469, 1.1691872003893877e-7, 0.19509033858776093),
+  scale: new Vector3(1.5000076293945312, 1.1997764110565186, 1.5000076293945312)
+})
+chairHighBack7.addComponentOrReplace(transform48)
+
+const chairHighBack8 = new Entity('chairHighBack8')
+engine.addEntity(chairHighBack8)
+chairHighBack8.setParent(_scene)
+chairHighBack8.addComponentOrReplace(gltfShape9)
+const transform49 = new Transform({
+  position: new Vector3(10, 4.5, 12),
+  rotation: new Quaternion(1.195820997901124e-15, -0.8314696550369263, 9.911889975455779e-8, -0.5555703043937683),
+  scale: new Vector3(1.5000014305114746, 1.197467565536499, 1.5000014305114746)
+})
+chairHighBack8.addComponentOrReplace(transform49)
+
+const chairHighBack9 = new Entity('chairHighBack9')
+engine.addEntity(chairHighBack9)
+chairHighBack9.setParent(_scene)
+chairHighBack9.addComponentOrReplace(gltfShape9)
+const transform50 = new Transform({
+  position: new Vector3(14, 4.5, 6.5),
+  rotation: new Quaternion(-1.0889569919349164e-15, -0.5555702447891235, 6.622912707143769e-8, 0.8314696550369263),
+  scale: new Vector3(1.500002145767212, 1.3803800344467163, 1.500002145767212)
+})
+chairHighBack9.addComponentOrReplace(transform50)
+
+const chairHighBack10 = new Entity('chairHighBack10')
+engine.addEntity(chairHighBack10)
+chairHighBack10.setParent(_scene)
+chairHighBack10.addComponentOrReplace(gltfShape9)
+const transform51 = new Transform({
+  position: new Vector3(13, 4.5, 5.5),
+  rotation: new Quaternion(4.440840747128199e-16, 0.19509033858776093, -2.3256578884911505e-8, 0.9807853698730469),
+  scale: new Vector3(1.5000003576278687, 1.2774569988250732, 1.5000003576278687)
+})
+chairHighBack10.addComponentOrReplace(transform51)
+
+const chairHighBack11 = new Entity('chairHighBack11')
+engine.addEntity(chairHighBack11)
+chairHighBack11.setParent(_scene)
+chairHighBack11.addComponentOrReplace(gltfShape9)
+const transform52 = new Transform({
+  position: new Vector3(12, 4.5, 6.5),
+  rotation: new Quaternion(1.195820997901124e-15, -0.8314696550369263, 9.911889975455779e-8, -0.5555703043937683),
+  scale: new Vector3(1.5000014305114746, 1.197467565536499, 1.5000014305114746)
+})
+chairHighBack11.addComponentOrReplace(transform52)
+
+const chairHighBack12 = new Entity('chairHighBack12')
+engine.addEntity(chairHighBack12)
+chairHighBack12.setParent(_scene)
+chairHighBack12.addComponentOrReplace(gltfShape9)
+const transform53 = new Transform({
+  position: new Vector3(13, 4.5, 7.5),
+  rotation: new Quaternion(-5.725116442799024e-15, -0.9807853698730469, 1.1691872003893877e-7, 0.19509033858776093),
+  scale: new Vector3(1.5000076293945312, 1.1997764110565186, 1.5000076293945312)
+})
+chairHighBack12.addComponentOrReplace(transform53)
+
 const channelId = Math.random().toString(16).slice(2)
 const channelBus = new MessageBus()
 const inventory = createInventory(UICanvas, UIContainerStack, UIImage)
@@ -399,11 +627,23 @@ const script1 = new Script1()
 const script2 = new Script2()
 const script3 = new Script3()
 const script4 = new Script4()
+const script5 = new Script5()
+const script6 = new Script6()
+const script7 = new Script7()
+const script8 = new Script8()
 script1.init(options)
 script2.init(options)
 script3.init(options)
 script4.init(options)
+script5.init(options)
+script6.init(options)
+script7.init(options)
+script8.init(options)
 script1.spawn(instagramButtonLink, {"url":"decentraland_art","bnw":true,"name":"https://www.google.fr/"}, createChannel(channelId, instagramButtonLink, channelBus))
 script2.spawn(facebookButtonLink, {"url":"decentraland","bnw":false}, createChannel(channelId, facebookButtonLink, channelBus))
 script3.spawn(twitterButtonLink, {"url":"decentraland","bnw":false}, createChannel(channelId, twitterButtonLink, channelBus))
 script4.spawn(videoScreenStanding, {"startOn":false,"onClickText":"Play video","volume":1,"onClick":[{"entityName":"videoScreenStanding","actionId":"toggle","values":{}}]}, createChannel(channelId, videoScreenStanding, channelBus))
+script5.spawn(galleryInfoDarkGrey2, {"text":"info natixis recrutement","fontSize":5,"font":"SF","color":"#000000"}, createChannel(channelId, galleryInfoDarkGrey2, channelBus))
+script6.spawn(externalLink, {"url":"https://recrutement.natixis.com/"}, createChannel(channelId, externalLink, channelBus))
+script7.spawn(plainText, {"text":"Welcome on \nNatixis Building","font":"SF","color":"#FFFFFF"}, createChannel(channelId, plainText, channelBus))
+script8.spawn(galleryInfoBlack, {"text":"Information natixis evenement\n","fontSize":5,"font":"SF","color":"#000000"}, createChannel(channelId, galleryInfoBlack, channelBus))
